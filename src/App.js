7a9b88ace8f6257c6ce9dabd259432a7c3ee9c11
putao0513 <2734322488@qq.com>
2025-06-13 02:15:11 +0000

    修改app文件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import pinyin from 'pinyin'; // 引入拼音库
 
-const WEATHER_API_KEY = '7b79783eaa789574bdc8f5299116acae'; // ← 换成你自己的 key！
+const WEATHER_API_KEY = '7b79783eaa789574bdc8f5299116acae'; // ← 替换成你自己的 key！
 
 function App() {
   const [city, setCity] = useState('');
@@ -19,15 +19,20 @@ function App() {
 
     let cityToSearch = city.trim();
     const isChinese = /[\u4e00-\u9fa5]/.test(cityToSearch);
+
+    // 中文 → 拼音
     if (isChinese) {
       const pyArr = pinyin(cityToSearch, { style: pinyin.STYLE_NORMAL });
       cityToSearch = pyArr.flat().join('');
     }
 
+    // 加上国家代码
+    const query = `${cityToSearch},cn`;
+
     setWeatherError('');
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=${WEATHER_API_KEY}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${WEATHER_API_KEY}&units=metric`)
       .then(res => {
-        if (!res.ok) throw new Error('城市不存在或拼写错误。请确认城市名拼写，或尝试输入拼音');
+        if (!res.ok) throw new Error('城市不存在或拼写错误。请确认拼写，或尝试输入拼音');
         return res.json();
       })
       .then(data => setWeather(data))
